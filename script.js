ea151d390f53555651adfbf4d80625a1a1616af6
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-06-17 15:44:10 +1200

    Create three variables to update display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//create function
+//create basic function
 
 // 1 Add
 const addNum = (num1, num2) => num1 + num2;
@@ -11,3 +11,7 @@ const multiplyNum = (num1, num2) => num1 * num2;
 
 // 4 Divide
 const divideNum = (num1, num2) => num1 / num2;
+
+let num1 = 0;
+let operator = 0;
+let num2 = 0;
