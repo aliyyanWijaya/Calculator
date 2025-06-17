f890cd44ce9f8426c5fc307ce5d21706af1f820e
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-06-17 15:38:50 +1200

    Add the function add, subtract, multiply and divide for two parameters

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+//create function
+
+// 1 Add
+const addNum = (num1, num2) => num1 + num2;
+
+// 2 Substract
+const subtractNum = (num1, num2) => num1 - num2;
+
+// 3 Multiply
+const multiplyNum = (num1, num2) => num1 * num2;
+
+// 4 Divide
+const divideNum = (num1, num2) => num1 / num2;
